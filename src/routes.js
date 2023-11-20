08dc49587fac0dc9cab8baa4fe6cafd1ce4685f6
petersonhz25 <mahez.pradana2070@students.unila.ac.id>
2023-11-20 10:37:13 +0700

    init

---
+++ src/routes.js
@@ -0,0 +1,30 @@
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler } = require('./handler')
+const routes = [
+  {
+    path: '/notes',
+    method: 'POST',
+    handler: addNoteHandler
+  },
+  {
+    path: '/notes',
+    method: 'GET',
+    handler: getAllNotesHandler
+  },
+  {
+    path: '/notes/{id}',
+    method: 'GET',
+    handler: getNoteByIdHandler
+  },
+  {
+    path: '/notes/{id}',
+    method: 'PUT',
+    handler: editNoteByIdHandler
+  },
+  {
+    path: '/notes/{id}',
+    method: 'DELETE',
+    handler: deleteNoteByIdHandler
+  }
+]
+
+module.exports = routes
